673c23aa930754e845c0bd0d32883f38ed8c5196
Jovan <jovan.najdovski@outlook.com>
2023-02-20 04:04:25 +0100

    fix: remove comments

--- src/sys/Pool.js
+++ src/sys/Pool.js
@@ -28,42 +28,30 @@ export default class Pool {
     }
 
     release(poolObject) {
-        // flag as free
         poolObject.free = true;
 
-        // set in the queue
         poolObject.nextFree = null;
         poolObject.previousFree = this.lastFree;
 
-        // if we had a last free, set the last free's next as the new poolObject
-        // otherwise, this is the first free!
         if (poolObject.previousFree) {
             this.lastFree.nextFree = poolObject;
         } else {
             this.nextFree = poolObject;
         }
 
-        // set the new object as the last in the queue
         this.lastFree = poolObject;
 
-        // reset the object if needed
         this.objReseter(poolObject);
     }
 
     getFree() {
-        // if we have a free one, get it - otherwise create it
         const freeObject = this.nextFree ? this.nextFree : this.addNewObject(this.newPoolObject());
-
-        // flag as used
         freeObject.free = false;
 
-        // the next free is the object's next free
         this.nextFree = freeObject.nextFree;
 
-        // if there's nothing afterwards, the lastFree is null as well
         if (!this.nextFree) this.lastFree = null;
 
-        // return the now not free object
         return freeObject;
     }
 
